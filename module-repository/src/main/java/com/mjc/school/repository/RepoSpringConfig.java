440f89a25e243a39ff8b7cad1612a1476a95ecba
Anatolii Zenin <anatolii.zenin@gmail.com>
2023-10-09 21:23:25 +0200

    fix repo spring config

--- module-repository/src/main/java/com/mjc/school/repository/RepoSpringConfig.java
+++ module-repository/src/main/java/com/mjc/school/repository/RepoSpringConfig.java
@@ -1,7 +1,6 @@
 package com.mjc.school.repository;
 
 import org.springframework.context.annotation.*;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -35,7 +34,7 @@ public class RepoSpringConfig {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
 //        dataSource.setUrl("jdbc:h2:mem:test1;INIT=RUNSCRIPT FROM 'classpath:schema.sql'");
-        dataSource.setUrl("jdbc:h2:file:./testdb;INIT=RUNSCRIPT FROM 'classpath:schema.sql'");
+        dataSource.setUrl("jdbc:h2:file:./testdb");
         dataSource.setUsername("sa");
         dataSource.setPassword("");
         return dataSource;
