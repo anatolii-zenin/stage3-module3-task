f64ad3326bd2d7183a8e02c0f57fbe635dc4953b
Anatolii Zenin <anatolii.zenin@gmail.com>
2023-10-12 13:23:00 +0200

    prepare for tags

--- module-service/src/main/java/com/mjc/school/service/validator/ValidatorAspect.java
+++ module-service/src/main/java/com/mjc/school/service/validator/ValidatorAspect.java
@@ -2,6 +2,7 @@ package com.mjc.school.service.validator;
 
 import com.mjc.school.service.dto.AuthorDTOReq;
 import com.mjc.school.service.dto.NewsDTOReq;
+import com.mjc.school.service.dto.TagDTOReq;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -19,18 +20,18 @@ public class ValidatorAspect {
 
     @Before("validate() && args(arg)")
     public void validateReq(Object arg) {
-        System.out.println("Validating DTO request input...");
         var objClass = arg.getClass();
         Validator validator;
         if (objClass.equals(NewsDTOReq.class))
             validator = applicationContext.getBean(NewsValidator.class);
         else if (objClass.equals(AuthorDTOReq.class))
             validator = applicationContext.getBean(AuthorValidator.class);
+        else if (objClass.equals(TagDTOReq.class))
+            return;
         else
             throw new RuntimeException("Validator unable to process class: " + objClass);
         if (!validator.validate(arg))
             throw new RuntimeException("Validation failed");
-        System.out.println("Validation passed.");
     }
 
     @Pointcut("execution(public * *(.., @com.mjc.school.service.validator.Validate (*), ..))")
