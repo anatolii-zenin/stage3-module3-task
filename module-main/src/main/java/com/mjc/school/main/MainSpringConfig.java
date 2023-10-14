f3e796e50a5320d3374c498c893519d65094142c
Anatolii Zenin <anatolii.zenin@gmail.com>
2023-10-14 20:10:53 +0200

    try to satisfy tests - insert some annotations to main

--- module-main/src/main/java/com/mjc/school/main/MainSpringConfig.java
+++ module-main/src/main/java/com/mjc/school/main/MainSpringConfig.java
@@ -2,8 +2,12 @@ package com.mjc.school.main;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @ComponentScan({"com.mjc.school.main", "com.mjc.school.controller"})
+@EnableJpaAuditing
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class MainSpringConfig {
 }
