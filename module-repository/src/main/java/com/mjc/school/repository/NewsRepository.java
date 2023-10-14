c882ffd6446d366df4330ebfb336819faf3334b8
Anatolii Zenin <anatolii.zenin@gmail.com>
2023-10-14 18:10:08 +0200

    implement reading news by criteria, fix readAll and readById

--- module-repository/src/main/java/com/mjc/school/repository/NewsRepository.java
+++ module-repository/src/main/java/com/mjc/school/repository/NewsRepository.java
@@ -2,5 +2,8 @@ package com.mjc.school.repository;
 
 import com.mjc.school.repository.model.implementation.NewsEntity;
 
+import java.util.List;
+
 public interface NewsRepository extends BaseRepository<NewsEntity, Long>{
+    List<NewsEntity> readNewsByCriteria(NewsEntity req);
 }
--- module-repository/src/main/java/com/mjc/school/repository/implementation/NewsRepositoryImpl.java
+++ module-repository/src/main/java/com/mjc/school/repository/implementation/NewsRepositoryImpl.java
@@ -6,8 +6,13 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityGraph;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -47,7 +52,7 @@ public class NewsRepositoryImpl extends AbstractBaseRepositoryImpl<NewsEntity>
     @Transactional(readOnly = true)
     public List<NewsEntity> readAll() {
         var findAll = getEntityManager().createQuery("" +
-                "SELECT a FROM " + getTableName() + " a " +
+                "SELECT DISTINCT a FROM " + getTableName() + " a " +
                 "LEFT JOIN FETCH a.tags"
         );
         return findAll.getResultList();
@@ -57,7 +62,7 @@ public class NewsRepositoryImpl extends AbstractBaseRepositoryImpl<NewsEntity>
     @Transactional(readOnly = true)
     public Optional<NewsEntity> readById(Long id) {
         var findOne = getEntityManager().createQuery("" +
-                "SELECT a FROM " + getTableName() + " a " +
+                "SELECT DISTINCT a FROM " + getTableName() + " a " +
                 "LEFT JOIN FETCH a.tags " +
                 "WHERE a.id=:id"
         );
@@ -67,5 +72,57 @@ public class NewsRepositoryImpl extends AbstractBaseRepositoryImpl<NewsEntity>
             return Optional.empty();
         var result = (NewsEntity) findList.get(0);
         return Optional.ofNullable(result);
+    }
+
+    @Override
+    @Transactional
+    public List<NewsEntity> readNewsByCriteria(NewsEntity req) {
+        //TODO: refactor
+        var criteriaBuilder = entityManager.getCriteriaBuilder();
+        var criteriaQuery = criteriaBuilder.createQuery(NewsEntity.class);
+        Root<NewsEntity> news = criteriaQuery.from(NewsEntity.class);
+        var newsAuthors = news.join("author", JoinType.LEFT);
+        var newsTags = news.join("tags", JoinType.LEFT);
+
+        EntityGraph<NewsEntity> fetchGraph = getEntityManager().createEntityGraph(NewsEntity.class);
+        fetchGraph.addSubgraph("author");
+        fetchGraph.addSubgraph("tags");
+
+        var predicates = new ArrayList<Predicate>();
+
+        var authorName = req.getAuthor().getName();
+        var authorId = req.getAuthor().getId();
+
+        if (authorName != null || authorId != null) {
+            if (authorName != null) {
+                predicates.add(criteriaBuilder.equal(newsAuthors.get("name"), authorName));
+            } else {
+                predicates.add(criteriaBuilder.equal(newsAuthors.get("id"), authorId));
+            }
+        }
+
+        var newsTitle = req.getTitle();
+        var newsContent = req.getContent();
+
+        if (newsTitle != null)
+            predicates.add(criteriaBuilder.like(news.get("title"), "%" + newsTitle + "%"));
+        if (newsContent != null)
+            predicates.add(criteriaBuilder.like(news.get("content"), "%" + newsContent + "%"));
+
+        var tags = req.getTags();
+        if (tags.size() > 0) {
+            for (var tag : tags) {
+                if (tag.getName() != null)
+                    predicates.add(criteriaBuilder.equal(newsTags.get("name"), tag.getName()));
+                if (tag.getId() != null)
+                    predicates.add(criteriaBuilder.equal(newsTags.get("id"), tag.getId()));
+            }
+        }
+
+        criteriaQuery.select(news).where(predicates.toArray(Predicate[]::new)).distinct(true);
+
+        var result =  entityManager.createQuery(criteriaQuery).setHint("javax.persistence.loadgraph", fetchGraph).getResultList();
+
+        return result;
     }
 }
