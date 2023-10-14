8b308bc4199dc06df6b774e1d706f691f0787ed5
Anatolii Zenin <anatolii.zenin@gmail.com>
2023-10-14 20:13:01 +0200

    undo last commit

--- module-main/src/main/java/com/mjc/school/main/MainSpringConfig.java
+++ module-main/src/main/java/com/mjc/school/main/MainSpringConfig.java
@@ -2,12 +2,8 @@ package com.mjc.school.main;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @ComponentScan({"com.mjc.school.main", "com.mjc.school.controller"})
-@EnableJpaAuditing
-@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class MainSpringConfig {
 }
